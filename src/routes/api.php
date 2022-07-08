2486a196d28ee656a8ca5a15f37a403fdd445303
ferdair <dairsanchez@gmail.com>
2022-07-08 03:40:49 -0500

    Created routes to personas y tickets

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\TicketController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/personas',[PersonaController::class, 'index']);
+Route::get('/persona/{id}', [PersonaController::class, 'show']);
+
+Route::get('/tickets',[TicketController::class, 'index']);
+Route::get('/ticket/{id}', [TicketController::class, 'show']);
+
+
